7a0221db909a64114e5569d128835952e8c63654
Ichicodes <ixchellopezalegre@gmail.com>
2021-01-25 12:11:34 +0100

    Update Cociente.java

--- src/com/itt/calculadora/Cociente.java
+++ src/com/itt/calculadora/Cociente.java
@@ -1,12 +1,12 @@
-package com.itt.calculadora;
+package cociente;
 /**
  * 
- * La clase Cociente contiene mÃ©todos para dividir dos operadores, 
- * realizar la inversiÃ³n de un nÃºmero, asÃ­ como la raiz cuadrada.
+ * La clase cociente contiene métodos para dividir dos operadores, 
+ * realizar la inversión de un número, así como la raiz cuadrada.
  * 
  * Esta clase pertenece al paquete {@link com.itt.calculadora}, 
- * reune varias clases para realizar distintas operaciones matemÃ¡ticas
- * @author Ixchel LÃ³pez Alegre
+ * reune varias clases para realizar distintas operaciones matemáticas
+ * @author Ixchel López Alegre
  * @since 18/01/2021
  * @version 1.0
  * @see com.itt.calculadora
@@ -20,66 +20,72 @@ public final class Cociente {
 	}
 	
 /**
- * Este mÃ©todo implementarÃ¡ la divisiÃ³n de dos nÃºmeros reales. Si
- * el segundo operando es igual a 0, la operaciÃ³n serÃ¡ nula.
+ * Función que realiza la división de dos números reales.
  * 
- * @param oper1 Representa el primer operador de la divisiÃ³n 
+ * @param oper1 Representa el primer operador de la división 
  * 
  * @param oper2 Representa el segundo operador, el dividendo
  * 
- * @return El resultado serÃ¡ la divisiÃ³n entre num1 y num2.
+ * @return Devuelve el valor de la división entre los dos operadores.
  * 
- 
+ * 
+ * Casos especiales:
+ * 	Si el segundo operador, es igual a 0, el resultado será NaN, POSITIVE_INFINITY o NEGATIVE_INFINITY.
+ * 	Si el ambos operadores son cero, el resultado sera NaN.
+ * 
+ *
  */
-	public static float cocienteReales (float oper1, float oper2) {
+	public static float dividirReales (float oper1, float oper2) {
 		return 0.0F ;
 	}
 	
 /**
- * Este mÃ©todo implementarÃ¡ la divisiÃ³n de dos nÃºmeros enteros. Si
- * el segundo operando es igual a 0, la operaciÃ³n serÃ¡ nula.
+ * Este método implementará la división de dos números enteros. 
  * 
- * @param oper1 Representa el primer operador de la divisiÃ³n 
+ * @param oper1 Representa el primer operador de la división 
  * 
  * @param oper2 Representa el segundo operador, el dividendo
  *
- * @return El resultado serÃ¡ la divisiÃ³n entre num1 y num2.
+ * @return El resultado será la división entre oper1 y oper2.
  * 
+ * @throws ArithmeticException Si el dividendo es igual a 0.
+ * 
+ * Casos especiales:
+ *  Si el segundo operador es igual a cero, el resultado será NaN.
  *
  */
-	public int cocienteEnteros (int oper1,int oper2) {
-		return 0;
+	public int dividirEnteros (int oper1,int oper2) {
+		return 0F;
 	}	
 	
 /**
- *  Este mÃ©todo implementarÃ¡ la inversiÃ³n de un nÃºmero entero.
+ *  Este método implementará la inversión de un número entero.
  *  
- *  @param oper1 Representa el nÃºmero que vas a invertir
+ *  @param oper1 Representa el número que vas a invertir
  *  
- *  @return El resultado serÃ¡ la inversiÃ³n del nÃºmero.
+ *  @return El resultado será la inversión del número.
+ *  
+ *  Ejemplo: si se introduce el valor 345, el método devolverá 543.
  * 
  */
 	public int invertirNum (int oper1) {
 
-		/*int inverso = 0, cifra = 0;
-		while (num1!=0) {
-			cifra= num1 % 10;
-			inverso = (inverso * 10) + cifra;
-			num1 /= 10;
-		}
-		return inverso;
-		*/
 		return 0;
 	}
 	
 /**
- * Este mÃ©todo implemetarÃ¡ la raiz cuadrada de un nÃºmero.
+ * Este método implemetará la raiz cuadrada de un número.
  * 
- * @param oper1 Representa el nÃºmero cuya raÃ­z cuadrada se va a calcular.
+ * @param oper1 Representa el número cuya raíz cuadrada se va a calcular.
  * 
- * @return Resultado de la raiz cuadrada del parÃ¡metro uno.
+ * @return Resultado de la raiz cuadrada del parámetro uno.
+ * 
+ * Casos especiales:
+ *  Si el operando es 0 el resultado es el mismo al operando.
+ *  Si el operando es NaN o menos de 0, el resultado es NaN. 
+ *  Si el argumento es infinito positivo, el resultado será infinito positivo.
  */
-	public double raizCuadrada (float oper1) {
+	public double raizCuadrada (double oper1) {
 		 return 0.0F;
 	}
 }
